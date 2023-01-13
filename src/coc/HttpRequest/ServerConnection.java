b8417887b1d569507319d8b7213298f687b3afcf
NicolasJott <nicobox2002@gmail.com>
2023-01-13 17:01:05 -0500

    Removed unused import

--- src/coc/HttpRequest/ServerConnection.java
+++ src/coc/HttpRequest/ServerConnection.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import com.google.gson.Gson;
 import org.json.JSONObject;
 
 public class ServerConnection {
