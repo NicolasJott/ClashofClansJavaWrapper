2f33dcd9bfaab4a97b6a6a26c20db15c72a6f141
NicolasJott <nicobox2002@gmail.com>
2023-01-13 23:38:30 -0500

    Made minimal changes

--- src/coc/Player/PlayerInfo.java
+++ src/coc/Player/PlayerInfo.java
@@ -7,30 +7,28 @@ import java.io.IOException;
 public class PlayerInfo {
 
     public static void main(String[] args) throws IOException {
-        String PLAYER_TAG = "QRRVVP99";
+        String PLAYER_TAG = "QUROQGQRG";
         String API_KEY = Tokens.getAPI_KEY();
-        Player otter = new Player(API_KEY, PLAYER_TAG);
-        Player cj = new Player (API_KEY, "V8VVOGUJ" );
-        Player berry = new Player (API_KEY, "GOR22PRL2" );
+        Player player = new Player(API_KEY, PLAYER_TAG);
 
-        System.out.println("Player Tag: " + berry.getPlayerTag());
-        System.out.println("Player Name: " + berry.getPlayerName());
-        System.out.println("TownHall Level: " + berry.getTownHallLevel());
+        System.out.println("Player Tag: " + player.getPlayerTag());
+        System.out.println("Player Name: " + player.getPlayerName());
+        System.out.println("TownHall Level: " + player.getTownHallLevel());
 
-        if (berry.getTownHallWeaponLevel() == 0) {
+        if (player.getTownHallWeaponLevel() == 0) {
             System.out.println("TownHall weapon Level: Not yet unlocked");
         } else {
-            System.out.println("TownHall Weapon Level: " + berry.getTownHallWeaponLevel());
+            System.out.println("TownHall Weapon Level: " + player.getTownHallWeaponLevel());
         }
 
-        System.out.println("Experience Level: " + berry.getExpLevel());
-        System.out.println("Trophies: " + berry.getTrophies());
-        System.out.println("Best Trophies: " + berry.getBestTrophies());
-        System.out.println("Total War Stars: " + berry.getTotalWarStars());
-        System.out.println("Total Capital Contributions: " + berry.getClanCapitalContributions());
-        System.out.println("Clan Tag: " + berry.getClanTag());
-        System.out.println("Clan Name: " + berry.getClanName());
-        System.out.println("Clan Badge: " + berry.getLargeClanBadge());
+        System.out.println("Experience Level: " + player.getExpLevel());
+        System.out.println("Trophies: " + player.getTrophies());
+        System.out.println("Best Trophies: " + player.getBestTrophies());
+        System.out.println("Total War Stars: " + player.getTotalWarStars());
+        System.out.println("Total Capital Contributions: " + player.getClanCapitalContributions());
+        System.out.println("Clan Tag: " + player.getClanTag());
+        System.out.println("Clan Name: " + player.getClanName());
+        System.out.println("Clan Badge: " + player.getLargeClanBadge());
 
 
 
